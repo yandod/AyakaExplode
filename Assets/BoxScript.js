b96fa3707919dcd74d3ee5dcf9c40464a2989253
Yusuke Ando <yandod@gmail.com>
2014-08-04 22:31:27 +0900

    initial commit

---
+++ Assets/BoxScript.js
@@ -0,0 +1,29 @@
+﻿
+function Start () {
+
+}
+
+function Update () {
+
+	//this.GetComponent('Detonator').Explode();
+	if (Input.GetKeyDown(KeyCode.Space)) {
+	var script = gameObject.GetComponent('Detonator');
+	script.Explode();
+	}
+	
+	if (transform.position.y < -30) {
+		rigidbody.velocity = Vector3.zero;
+		transform.rotation = Quaternion.identity;
+		transform.position = new Vector3(0,5,0);
+	}
+}
+
+function OnCollisionEnter(other:Collision) {
+
+	if (other.gameObject.tag == 'safe') {
+		return;
+	}
+	//var script = gameObject.GetComponent('Detonator');
+	//script.Explode();
+
+}
